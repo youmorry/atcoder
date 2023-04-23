d51cb1bfd02bbae6305d036877f1e4606067e702
yumori <youmorry@gmail.com>
2023-04-24 06:24:01 +0900

    abc051_b:AC

---
+++ abc051/abc051_b/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int k, s;
+    cin >> k >> s;
+
+    int ans = 0;
+    for (int x = 0; x <= k; x++) {
+        for (int y = 0; y <= k; y++) {
+            int z = s - x - y;
+            if (z < 0 || z > k) continue;
+            ++ans;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
