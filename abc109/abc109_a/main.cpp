f06d7367f69cb3ccfb80ba380355ce7c4455de02
yumori <youmorry@gmail.com>
2023-05-05 14:32:16 +0900

    abc109_a:AC

---
+++ abc109/abc109_a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a*b % 2 == 1) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
