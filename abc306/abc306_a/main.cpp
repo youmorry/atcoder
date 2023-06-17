5172e034ee88293cdfd0d1eed2b4258c36a7bcff
yumori <youmorry@gmail.com>
2023-06-18 07:01:16 +0900

    abc306_a:AC

---
+++ abc306/abc306_a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int n; cin >> n;
+    string s; cin >> s;
+    for (auto c : s) cout << c << c;
+    cout << endl;
+    return 0;
+}
