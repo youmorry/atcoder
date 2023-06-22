7aa62428d50a8a7211466063eccf428b7fd6af93
yumori <youmorry@gmail.com>
2023-06-23 08:40:35 +0900

    Modified

--- template.cpp
+++ template.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 
 int main() {
     return 0;
