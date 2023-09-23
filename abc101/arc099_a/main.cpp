cbd9d282fe3022d06116bb15a4292555270ff57d
yumori <youmorry@gmail.com>
2023-09-23 20:51:59 +0900

    arc099_a:解説AC

---
+++ abc101/arc099_a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    cout <<  (n - 1 + k - 2) / (k - 1) << endl;
+    return 0;
+}
