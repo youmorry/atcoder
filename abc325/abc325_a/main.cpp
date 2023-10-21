2fe8428d4a2d2cf4c89f974114f77600c65004e7
yumori <youmorry@gmail.com>
2023-10-22 08:40:06 +0900

    abc325_a:AC

---
+++ abc325/abc325_a/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<pair<int, int>>>;
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+    cout << s << " " << "san" << endl;
+    return 0;
+}
